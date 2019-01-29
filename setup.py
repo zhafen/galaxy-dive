c83f58a62c0003e722876b74165273363989bd46
Zach Hafen <zachary.h.hafen@gmail.com>
2019-01-29 11:26:58 -0600

    Updated installation files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='galaxy_dive',
-    version='0.8.2',
+    version='0.9.0',
     description='A general analysis suite for hydrodynamic galaxy simulations.',
     url='https://github.com/zhafen/galaxy-dive',
     author='Zach Hafen',
@@ -12,18 +12,21 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     install_requires=[
-        'numpy>=1.14.5',
         'pandas>=0.20.3',
         'mock>=2.0.0',
+        'numpy>=1.14.5',
         'pytest>=3.4.0',
         'unyt>=1.0.4',
-        'colossus>=1.2.1',
+        'six>=1.10.0',
         'setuptools>=28.8.0',
+        'colossus>=1.2.2',
         'matplotlib>=2.0.2',
         'h5py>=2.7.0',
-        'scipy>=1.1.0',
+        'scipy>=1.2.0',
+        'verdict>=1.0',
     ],
     include_package_data=True,
 )
