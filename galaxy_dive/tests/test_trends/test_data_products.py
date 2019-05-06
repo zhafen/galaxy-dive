cecb37ad3263840f507f15ac751dee36aadc73b7
Zach Hafen <zachary.h.hafen@gmail.com>
2019-05-06 13:56:16 -0500

    Added a function for loading Mike's tidal tensor data.

---
+++ galaxy_dive/tests/test_trends/test_data_products.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''Testing for trends.cgm.py
+
+@author: Zach Hafen
+@contact: zachary.h.hafen@gmail.com
+@status: Development
+'''
+
+import glob
+from mock import call, patch
+import numpy as np
+import numpy.testing as npt
+import os
+import pdb
+import pytest
+import unittest
+
+import galaxy_dive.trends.data_products as data_products
+
+########################################################################
+########################################################################
+
+class TestDataProducts( unittest.TestCase ):
+
+    def test_tidal_tensor_data_grudic( self ):
+        '''Just test that we're reading the data correctly.
+        '''
+
+        actual = data_products.tidal_tensor_data_grudic(
+            100,
+            ids = np.array([ 48850256, 9550185, 1111111111111 ]),
+        )
+        # From manually looking at the data
+        expected = np.array([
+            [ 1.22668942e+03,  7.75438811e+01,  6.02076014e+02, -3.91015235e+01, -1.17870247e+02,  7.95706269e+01,  8.74140108e+01, 1.31098915e-01,  6.59692747e+03 ],
+            [ 1.00920133e+01,  4.79382207e+01,  2.02634658e+01, 6.64207692e+00,  2.51895674e+01, -1.50385106e+01,  3.50740113e+01, 1.26965516e+00,  5.80723533e+01 ],
+            [ np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, ],
+        ])
+
+        npt.assert_allclose( expected, actual.values )
+            
+    ########################################################################
---
+++ galaxy_dive/trends/data_products.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+'''Compilation of functions for interfacing with miscellanious data products.
+
+@author: Zach Hafen
+@contact: zachary.h.hafen@gmail.com
+@status: Development
+'''
+
+import numpy as np
+import os
+import pandas as pd
+
+########################################################################
+
+def tidal_tensor_data_grudic(
+    snum,
+    ids = None,
+    data_dir = '/work/03532/mgrudic/tidal_tensor',
+):
+    '''Load data Mike Grudic processed that contains Tidal Tensor, velocity
+    dispersion, and items used for calculating the aforementioned quantities.
+
+    Args:
+        snum (int): Snapshot to retrieve the data for.
+
+        ids (array-like): IDs to retrieve. Defaults to all.
+
+        data_dir (str): Path to directory containing data.
+
+    Returns:
+        pandas.DataFrame
+            DataFrame containing quantities. When given an ID not in the data
+            returns NaN values for that ID.
+    '''
+
+    # Load the data
+    filename = 'tidal_tensor_{}.npy'.format( snum )
+    file_path = os.path.join( data_dir, filename )
+    full_arr = np.load( file_path )
+    
+    # Convert to a pandas data frame to get the selected IDs out.
+    data = {
+        'ID': full_arr[:,0].astype( int ),
+        'Txx': full_arr[:,1],
+        'Tyy': full_arr[:,2],
+        'Tzz': full_arr[:,3],
+        'Txy': full_arr[:,4],
+        'Tyz': full_arr[:,5],
+        'Tzx': full_arr[:,6],
+        'sigma_v': full_arr[:,7],
+        'r_search': full_arr[:,8],
+        'cond_num': full_arr[:,9],
+    }
+    df = pd.DataFrame( data, )
+    df = df.set_index( 'ID' )
+
+    # Select on IDs
+    if ids is not None:
+        df = df.loc[ids]
+
+    return df
