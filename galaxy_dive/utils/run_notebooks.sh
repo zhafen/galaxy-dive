b315c33e70c67840c7016fc9db4900943aa4d4dd
Zach Hafen <zachary.h.hafen@gmail.com>
2019-05-10 13:59:47 -0500

    Wrap-up for fates viz.

--- galaxy_dive/utils/run_notebooks.sh
+++ galaxy_dive/utils/run_notebooks.sh
@@ -19,8 +19,8 @@ nbs_to_run=(*.ipynb)
 for nb in ${nbs_to_run[@]};
 do
     # This doesn't work with Python2.7 (assumed default, so switch to Python 3)
-    module unload python
-    module load python3
+    # module unload python
+    # module load python3
 
     # Convert NBs
     jupyter nbconvert --to python $nb
@@ -28,9 +28,9 @@ do
     echo Converted $nb to $nb_script ...
 
     # Switch back to Python 2
-    module load python/2.7.13
-    module unload python3
-    module load phdf5
+    # module load python/2.7.13
+    # module unload python3
+    # module load phdf5
 
     # And now run the NBs
     echo Running $nb_script ...
